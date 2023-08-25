aa0b1b9bf4db89a6023b4d49e90f314c778a755a
Policarpopvp <joaovader99@gmail.com>
2023-08-25 09:07:20 -0300

    joaopolicarpo

--- app.js
+++ app.js
@@ -5,3 +5,6 @@ http.createServer(function (req, res) {
   res.end('meu primeiro app NodeJS');
   console.log('servidor rodando em http://localhost:8080/')
 }).listen(8080); 
+
+console.log('This example is different!');
+console.log('The result is displayed in the Command Line Interface');
